0edcac5d86c60def5afb07ac580e891d8356cdda
Etan Joseph Heyman <etan@heyman.net>
2025-02-26 20:40:05 -0600

    Nav layout and background svg ready for finishes

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Logo from './Logo'
 
 const Nav = () => {
   return (
-    <div className='w-full h-15'>Nav</div>
+    
+    <nav className='w-full h-full z-10 top-0 bg-background  px-4.5 pb-4 pt-3 sticky'>
+        <div className='flex h-15   rounded-[80px] items-center bg-white justify-center'>
+            <div className='flex items-center justify-center gap-2'>
+                <Logo />
+            </div>
+        </div>
+    </nav>
   )
 }
 
