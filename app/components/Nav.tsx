c0735fe56e48daf93cc2a6aed7ee304683dfec26
Etan Joseph Heyman <etan@heyman.net>
2025-03-01 19:46:52 -0600

    Add glow to E svg

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,8 +4,8 @@ import Logo from './Logo'
 const Nav = () => {
   return (
     
-    <nav className='w-full h-full z-10 top-0 bg-background  px-4.5 pb-4 pt-3 sticky'>
-        <div className='flex h-15   rounded-[80px] items-center bg-white justify-center'>
+    <nav className='w-full h-full z-10 top-0 bg-background px-4.5 pb-4 pt-3 sticky'>
+        <div className='flex h-15 rounded-[80px] items-center bg-white justify-center'>
             <div className='flex items-center justify-center gap-2'>
                 <Logo />
             </div>
