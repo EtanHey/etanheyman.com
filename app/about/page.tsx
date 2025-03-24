d7d53cf52b6589c8e28bf93fb6d1d49ca20863f6
Etan Joseph Heyman <etan@heyman.net>
2025-03-23 18:39:17 -0600

    About page mobile design done.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -46,7 +46,7 @@ const AboutPage = () => {
   }, [isInitialized, careerSectionPosition]);
 
   return (
-    <div className='flex w-full flex-col z-20 items-start justify-items-center min-h-screen px-4.5 py-8 gap-12 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+    <div className='flex w-full flex-col z-20 items-start justify-items-center min-h-screen px-4.5 py-8 gap-20 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <div className='flex flex-col gap-4'>
         <h1 className='text-[34px] pb-2 leading-5.5 font-bold'>Etan Heyman</h1>
         <div className='flex items-center gap-4'>
@@ -71,11 +71,11 @@ const AboutPage = () => {
       </div>
 
       {/* Career Journey Section */}
-      <div ref={careerSectionRef} className='w-full flex flex-col gap-2 mt-8 pt-4' id='career-section'>
+      <div ref={careerSectionRef} className='w-full flex flex-col gap-2  pt-4' id='career-section'>
         <h2 className='text-2xl text-blue-200 font-bold'>My career journey</h2>
         <TimelineParallax />
       </div>
-      <div className='flex flex-col pb-20 gap-6'>
+      <div className='flex flex-col pb-12 w-full justify-center items-center gap-6'>
         <div className='flex flex-col gap-2'>
           <h2 className='text-2xl text-blue-200 font-bold'>Like what you see?</h2>
           <p className='text-[22px] pr-5 leading-5.5 font-light'>Don’t hesitate to contact me right away!</p>
